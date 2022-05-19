a320e353aae495b0f144997f32bc0b86e6827092
JYP <wldb0208@naver.com>
2022-05-20 05:14:15 +0900

    emer2

--- websocket/index.js
+++ websocket/index.js
@@ -42,7 +42,8 @@
      // 3) 클라이언트로부터 메시지 수신 이벤트 처리 
      ws.on('message', (msg)=>{ 
         console.log(`클라이언트[${ip}]에게 수신한 메시지 : ${msg}`); 
-        ws.send(msg); 
+        webSocket.send(msg);
+        // ws.send(msg); 
         //  ws.send('메시지 잘 받았습니다! from 서버'); 
      }) 
      
